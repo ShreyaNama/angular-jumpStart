7fff45b2ade3eeea9b3879c1dd87248dd9581d45
Shreya Nama <shreya.nama1@t-mobile.com>
2017-12-19 16:42:47 +0530

    getting response from api

--- server.js
+++ server.js
@@ -3,17 +3,22 @@ const app = express(),
     bodyParser  = require('body-parser'),
     fs          = require('fs')
     customers   = JSON.parse(fs.readFileSync('data/customers.json', 'utf-8')),
-    states      = JSON.parse(fs.readFileSync('data/states.json', 'utf-8'));
+    //states      = JSON.parse(fs.readFileSync('data/states.json', 'utf-8'));
 	
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //The dist folder has our static resources (index.html, css, images)
-//app.use(express.static(__dirname + '/data')); 
+app.use(express.static(__dirname + '/dist')); 
 
 app.get('/api/customers/page/:skip/:top', (req, res) => {
    
     res.json(customers);
+});
+
+// redirect all others to the index (HTML5 history)
+app.all('/*', function(req, res) {
+    res.sendFile(__dirname + '/dist/index.html');
 });
 
 app.listen(4200, () => console.log('Example app listening on port 4200!'))
